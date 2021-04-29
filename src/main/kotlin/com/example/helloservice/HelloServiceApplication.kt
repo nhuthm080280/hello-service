e23cfd6bc38016c31bb48d34d408b35bcf3cd03f
nhuthuynh <nhuthm080280@gmail.com>
2021-04-29 14:35:41 +0700

    initial project

---
+++ src/main/kotlin/com/example/helloservice/HelloServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.helloservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HelloServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<HelloServiceApplication>(*args)
+}
